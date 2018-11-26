a0fec36fea06204a63dbb9131725de06e626d4d1
uuuser <uuu@uuusernomacbook-pro.local>
2018-11-26 13:05:16 +0900

    Initial Commit

---
+++ FlipByBlink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipByBlink
+//
+//  Created by uuuser on 2018/11/26.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
