c896316e9fd0c7f8bfadfc1b7b1d70eaec757bbc
uuuser <uuu@uuusernomacbook-pro.local>
2018-11-26 13:56:23 +0900

    firstCommit ud機能は一旦削除

--- FlipByBlink/AppDelegate.swift
+++ FlipByBlink/AppDelegate.swift
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.swift
-//  FlipByBlink
-//
-//  Created by uuuser on 2018/11/26.
-//  Copyright © 2018 user. All rights reserved.
-//
-
 import UIKit
+import PDFKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,28 +12,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        if let vc:ViewController = window?.rootViewController as? ViewController{
+            if let d = PDFDocument(url: url){
+                vc.pdfビュー.autoScales = true
+                vc.pdfビュー.displayMode = .singlePage
+                vc.pdfビュー.backgroundColor = .black
+                vc.pdfビュー.displaysPageBreaks = false
+                vc.pdfビュー.document = d
+                vc.pdfビュー.goToFirstPage(nil)
+            }
+        }
+        do{
+            try FileManager.default.removeItem(at: url)
+        }catch{
+            print("remove miss")
+        }
+        return true
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
--- FlipByBlink/ViewController.swift
+++ FlipByBlink/ViewController.swift
@@ -1,20 +1,116 @@
-//
-//  ViewController.swift
-//  FlipByBlink
-//
-//  Created by uuuser on 2018/11/26.
-//  Copyright © 2018 user. All rights reserved.
-//
+//諸設定
+//カメラプライバシ
+//DocumentTypePDF
+//hiddenStatusBar
+//upSideDown
 
 import UIKit
+import PDFKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,ARSessionDelegate,ARSCNViewDelegate  {
+    
+    @IBOutlet weak var pdfビュー: PDFView!
+    
+    @IBOutlet weak var sceneビュー: ARSCNView!
+    
+    var ひとつ前に検出された目の開け具合:Double = 0.0
+    var まばたきし始めた時刻:Date?
+    var まばたきし続けてる時刻:Date?
+    
+    let 瞼の開け具合の閾値:Double = 0.8
+    let 瞼を閉じ続ける時間の閾値:Double = 0.15
+    
+    var まだページ送りしてない:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        まばたきし始めた時刻 = Date()
+        まばたきし続けてる時刻 = Date()
+        
+        if let サンプルURL = Bundle.main.url(forResource: "サンプル", withExtension: "pdf") {
+            if let 開くPDF = PDFDocument(url: サンプルURL) {
+                pdfビュー.autoScales = true
+                pdfビュー.displayMode = .singlePage
+                pdfビュー.backgroundColor = .black
+                pdfビュー.displaysPageBreaks = false
+                pdfビュー.document = 開くPDF
+            }
+        }
+        
+        sceneビュー.delegate = self
+        sceneビュー.session.delegate = self
+        
+        let configuration = ARFaceTrackingConfiguration()
+        
+        sceneビュー.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        sceneビュー.isHidden = true
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+    }
+    @IBAction func nextページ(_ sender: Any) {
+        pdfビュー.goToNextPage(nil)
+    }
+    @IBAction func 高速でページ進める(_ sender: Any) {
+        pdfビュー.goToNextPage(nil)
+    }
+    @IBAction func previousページ(_ sender: Any) {
+        pdfビュー.goToPreviousPage(nil)
+    }
+    @IBAction func 高速でページ戻す(_ sender: Any) {
+        pdfビュー.goToPreviousPage(nil)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        let 左目の開け具合 = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue
+        
+        if 左目の開け具合! > 瞼の開け具合の閾値 && ひとつ前に検出された目の開け具合 < 瞼の開け具合の閾値{
+            まばたきし始めた時刻 = Date()
+        }
+        if 左目の開け具合! > 瞼の開け具合の閾値{
+            まばたきし続けてる時刻 = Date()
+            if まばたきし続けてる時刻!.timeIntervalSince(まばたきし始めた時刻!) > TimeInterval(瞼を閉じ続ける時間の閾値){
+                if まだページ送りしてない{
+                    DispatchQueue.main.async {
+                        self.pdfビュー.goToNextPage(nil)
+                    }
+                    
+                    まだページ送りしてない = false
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        self.まだページ送りしてない = true
+                    }
+                }
+            }
+        }
+        
+        ひとつ前に検出された目の開け具合 = 左目の開け具合!
+        
+    }
+    
+    override var keyCommands: [UIKeyCommand]?{
+        let commands = [UIKeyCommand(input: UIKeyCommand.inputRightArrow, modifierFlags: UIKeyModifierFlags.init(rawValue: 0), action: #selector(右矢印で次のページへ移動(command:))),
+                        UIKeyCommand(input: UIKeyCommand.inputLeftArrow, modifierFlags: UIKeyModifierFlags.init(rawValue: 0), action: #selector(左矢印で前のページへ移動(command:)))]
+        return commands
+    }
+    
+    @objc func 右矢印で次のページへ移動(command: UIKeyCommand) {
+        pdfビュー.goToNextPage(nil)
     }
 
-
+    @objc func 左矢印で前のページへ移動(command: UIKeyCommand) {
+        pdfビュー.goToPreviousPage(nil)
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
 }
 
